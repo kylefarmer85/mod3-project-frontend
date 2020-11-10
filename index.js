19e898cec294f9d9351aca0550feee63efd1e900
Justin_Webb <Webb.n.justin@gmail.com>
2020-11-10 15:20:57 -0600

    user frontend started

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 function main() {
-<<<<<<< HEAD
-  // fetchPosts()
-  fetchUser()
-  hideForm()
-=======
   fetchPosts()
   createPostFormEventListener()
   createEditPostFormEventListener()
-
->>>>>>> 1def0106b482a3a3510bfcd2414f5f51296cb649
+  fetchUser()
+  hideForm()
 }
 let currentPosts = []
 let currentUser;
@@ -25,14 +20,12 @@ const USERS_URL = "http://localhost:3000/api/v1/users"
 const POSTS_URL = "http://localhost:3000/api/v1/posts"
 const COMMENTS_URL = "http://localhost:3000/api/v1/comments"
 const postForm = qs('.post-form')
-<<<<<<< HEAD
-const usersContainer = qs('.users-container')
-=======
 const editPostForm = qs('.edit-post-form')
-const userLogin = qs('.user-form')
->>>>>>> 1def0106b482a3a3510bfcd2414f5f51296cb649
 const postsContainer = qs('.posts-container')
 const postFormContainer = qs('.post-form-container')
+
+const usersContainer = qs('.users-container')
+const userLogin = qs('.user-form')
 
 
 
@@ -116,47 +109,6 @@ function renderPost(post) {
 
   author.innerText = post.author
 
-<<<<<<< HEAD
-  postDiv.append(userName, postImage, author)
-  postsContainer.append(postDiv)
-
-}
-  
-//add delete btn dataset.id, add edit btn make here
-//create like btn, innerText = post.upvote
-//event listener for editbtn inside of render post. post.title, add attribute data-id post.id, post.image_url
-
-// const hiddenInput = ce("input")
-// hiddenInput.setAttribute("type", "hidden")
-// hiddenInput.value = post.id
-// hiddenInput.name = "id"
-
-// editForm.append(hiddenInput)
-
-// const editBtn = ce("button")
-// editBtn.innerText = "Edit Post"
-
-// editBtn.eventListner('click' (e) => {
-//   if (currentUser.id === post.user_id)
-//   // submitBtn.className = "Edit"
-//   postForm[0].value = post.title
-//   postForm[1].value = post.image_url
-//   postForm[2].value = post.content
-//   postForm[3].value = post.user_id
-//   postForm[4].value = 
-// })
-
-
-// postForm.addEventListener('submit' , )
-// // if submitBtn.className = "edit"
-// currentPosts.forEach(post => {
-//   if post.id === e.target[3].value
-//     fetch PATCH
-// //     submitBtn.clssName = "post"
-// // if submitBtn.className = "post"
-//   else
-//     fetch POST
-=======
   const editBtn = ce("button")
   editBtn.innerText = "Edit Post"
 
@@ -235,5 +187,4 @@ function createEditPostFormEventListener() {
 
 
 
->>>>>>> 1def0106b482a3a3510bfcd2414f5f51296cb649
 main()
