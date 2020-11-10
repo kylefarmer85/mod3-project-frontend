93644d19dcfb01c1fb45b5c6ab8d7e2f9f1111da
kylefarmer85 <kylefarmer85@gmail.com>
2020-11-10 11:54:10 -0600

    plan added, justin working on user form/login, kyle on post form

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function main() {
 
 }
 
+let currentUser;
+
 function ce(element){
   return document.createElement(element)
 }
@@ -14,15 +16,23 @@ function qs(selector){
 const USERS_URL = "http://localhost:3000/api/v1/users"
 const POSTS_URL = "http://localhost:3000/api/v1/posts"
 const COMMENTS_URL = "http://localhost:3000/api/v1/comments"
-
-
+const postForm = qs('.post-form')
+const userLogin = qs('.user-form')
 const postsContainer = qs('.posts-container')
 
-function fetchPosts() {
-  fetch(POSTS_URL)
-  .then(resp => resp.json())
-  .then(posts => posts.forEach(renderPost))
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function fetchPosts() {
   fetch(POSTS_URL)
@@ -31,7 +41,7 @@ function fetchPosts() {
 }
 
 function renderPost(post) {
- 
+  currentPosts << post
   const postDiv = ce('div')
   postDiv.className = "card"
   const titleH3 = ce('h3')
@@ -46,6 +56,41 @@ function renderPost(post) {
   postDiv.append(titleH3, postImage, author)
   postsContainer.append(postDiv)
 
-}
+//add delete btn dataset.id, add edit btn make here
+//create like btn, innerText = post.upvote
+//event listener for editbtn inside of render post. post.title, add attribute data-id post.id, post.image_url
+
+// const hiddenInput = ce("input")
+// hiddenInput.setAttribute("type", "hidden")
+// hiddenInput.value = post.id
+// hiddenInput.name = "id"
+
+// editForm.append(hiddenInput)
+
+const editBtn = ce("button")
+editBtn.innerText = "Edit Post"
+
+editBtn.eventListner('click' (e) => {
+  if (currentUser.id === post.user_id)
+  // submitBtn.className = "Edit"
+  postForm[0].value = post.title
+  postForm[1].value = post.image_url
+  postForm[2].value = post.content
+  postForm[3].value = post.user_id
+  postForm[4].value = 
+})
+
+
+postForm.addEventListener('submit' , )
+// if submitBtn.className = "edit"
+currentPosts.forEach(post => {
+  if post.id === e.target[3].value
+    fetch PATCH
+//     submitBtn.clssName = "post"
+// if submitBtn.className = "post"
+  else
+    fetch POST
+
+
 
 main()
--- notes.md
+++ notes.md
@@ -1,4 +1,35 @@
-create user login form 
+--create user login form 
+  -create html login form, hard code in index.html = 
+  -fetch one user and assign the newUser to the global JS user variable
+-window alert successfully logged in or not
 
--fetch one user and assign the newUser to the global JS user variable
--buttons and other user access elements hidden until user.id === true?
+  -buttons and other user access elements hidden until user.id === true?
+  After buttons created:
+    -if NOT user, hide these:
+    -create new post button
+    -create a new comment
+    -upvot a post
+    -access user profile
+  
+--create a post form
+  -create a new post button
+  -add event listener to button to render new post form that is listening for 'submit'
+  -dont make fields for user.id or author, will fill those in our self with the global user variable (currentUser.id, currentUser.username)
+  -on submit, make a fetch POST to posts url
+    body:
+    title: e.target[0].value
+    image_url: e.target[1].value
+    content: e.trget[2].value
+    github_url: e.target[3].value
+    user_id: currentUser.id
+    author: currentUser.username
+    upvote: 0
+  --post comes back from db, render post function
+
+  --render post function add delete button
+  -add new post form HTML to index.html
+  
+--append post.comments
+  --add comment comment button
+
+
