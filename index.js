1def0106b482a3a3510bfcd2414f5f51296cb649
kylefarmer85 <kylefarmer85@gmail.com>
2020-11-10 14:49:15 -0600

    added new post form and edit post form, added edit button to card

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function main() {
   fetchPosts()
+  createPostFormEventListener()
+  createEditPostFormEventListener()
 
 }
-
+let currentPosts = []
 let currentUser;
 
 function ce(element){
@@ -17,8 +19,11 @@ const USERS_URL = "http://localhost:3000/api/v1/users"
 const POSTS_URL = "http://localhost:3000/api/v1/posts"
 const COMMENTS_URL = "http://localhost:3000/api/v1/comments"
 const postForm = qs('.post-form')
+const editPostForm = qs('.edit-post-form')
 const userLogin = qs('.user-form')
 const postsContainer = qs('.posts-container')
+const postFormContainer = qs('.post-form-container')
+
 
 
 
@@ -41,7 +46,8 @@ function fetchPosts() {
 }
 
 function renderPost(post) {
-  currentPosts << post
+  currentPosts.push(post)
+
   const postDiv = ce('div')
   postDiv.className = "card"
   const titleH3 = ce('h3')
@@ -53,43 +59,81 @@ function renderPost(post) {
 
   author.innerText = post.author
 
-  postDiv.append(titleH3, postImage, author)
+  const editBtn = ce("button")
+  editBtn.innerText = "Edit Post"
+
+  postDiv.append(titleH3, postImage, author, editBtn)
   postsContainer.append(postDiv)
 
-//add delete btn dataset.id, add edit btn make here
-//create like btn, innerText = post.upvote
-//event listener for editbtn inside of render post. post.title, add attribute data-id post.id, post.image_url
 
-// const hiddenInput = ce("input")
-// hiddenInput.setAttribute("type", "hidden")
-// hiddenInput.value = post.id
-// hiddenInput.name = "id"
-
-// editForm.append(hiddenInput)
-
-const editBtn = ce("button")
-editBtn.innerText = "Edit Post"
-
-editBtn.eventListner('click' (e) => {
-  if (currentUser.id === post.user_id)
-  // submitBtn.className = "Edit"
-  postForm[0].value = post.title
-  postForm[1].value = post.image_url
-  postForm[2].value = post.content
-  postForm[3].value = post.user_id
-  postForm[4].value = 
-})
+  editBtn.addEventListener('click', () => {
+  // if (currentUser.id === post.user_id)
+    editPostForm[0].value = post.title
+    editPostForm[1].value = post.image_url
+    editPostForm[2].value = post.content
+    editPostForm[3].value = post.github_url
+    editPostForm[4].value = post.author
+    editPostForm[5].value = post.user_id
+    editPostForm[6].value = post.id
+  })
+}
 
 
-postForm.addEventListener('submit' , )
-// if submitBtn.className = "edit"
-currentPosts.forEach(post => {
-  if post.id === e.target[3].value
-    fetch PATCH
-//     submitBtn.clssName = "post"
-// if submitBtn.className = "post"
-  else
-    fetch POST
+function createPostFormEventListener() {
+
+  postForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    fetch (POSTS_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: e.target[0].value, 
+        image_url: e.target[1].value,
+        content: e.target[2].value,
+        github_url: e.target[3].value,
+        author: "this will be currentUser.username",       
+        user_id: 1,
+        upvote: 0
+      })
+    })
+    .then(resp => resp.json())
+    .then(newPost => renderPost(newPost))
+
+    e.target.reset()
+  })  
+}
+
+
+function createEditPostFormEventListener() {
+
+  editPostForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const postId = parseInt(e.target[6].value)
+
+    fetch (`${POSTS_URL}/${postId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: e.target[0].value,
+        image_url: e.target[1].value,
+        content: e.target[2].value,
+        github_url: e.target[3].value,
+        author: e.target[4].value,
+        user_id: e.target[5].value,
+        id: e.target[6].value
+      })
+    })
+    .then(resp => resp.json())
+    .then(console.log)
+
+    e.target.reset()
+  })
+}
 
 
 
